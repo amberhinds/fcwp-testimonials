e9a884a401a0117fc66cc68e6048468941cbf8be
David Hayes <dhayes@gmail.com>
2016-02-10 15:51:49 -0700

    It shows the title of a random testimonial

--- lib/shortcode.php
+++ lib/shortcode.php
@@ -2,5 +2,16 @@
 
 add_shortcode( 'fcwp_testimonial', 'fcwp_shortcode' );
 function fcwp_shortcode() {
-	return "I'm here! :p";
+	$args = array(
+		'post_type' => 'testimonial',
+		'orderby' => 'rand',
+	);
+	$query = new WP_Query($args);
+	
+	if ($query->have_posts()) {
+		while ($query->have_posts()) {
+			$query->the_post();
+			return get_the_title();
+		}
+	}
 }
