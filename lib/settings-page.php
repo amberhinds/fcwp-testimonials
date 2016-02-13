55741e1f7c0b536ff34658c30469cc5169f931e6
Mike Selander <mikeselander@gmail.com>
2016-02-13 10:00:33 -0700

    Added settings page file

---
+++ lib/settings-page.php
@@ -0,0 +1,32 @@
+<?php
+
+class FCWPSettingsPage(){
+
+	public function hooks(){
+
+
+
+	}
+
+
+	public function set_menu_page(){
+
+
+
+	}
+
+
+	public function register_fields(){
+
+
+
+	}
+
+
+	public function settings_page(){
+
+
+
+	}
+
+}
